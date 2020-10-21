1e81b2e1795582078b4230a5b36701122d733f05
ciweala <58187587+ciweala@users.noreply.github.com>
2020-10-21 15:44:44 -0400

    edits js 2

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -48,7 +48,6 @@ document.body.addEventListener('submit', async (e) => {
       const ul = document.createElement('ul');
       ul.className = 'flex-inner';
       $('form').prepend(ul);
-      reverseList.sort().reverse();
 
       reverseList.forEach((el, i) => {
         const li = document.createElement('li');
