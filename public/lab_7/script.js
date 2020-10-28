b8f34d7fe186277b856bf011e47f3d9bc4945950
ciweala <58187587+ciweala@users.noreply.github.com>
2020-10-28 00:22:48 -0400

    edits

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,52 +1,11 @@
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
+  
   return list;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  const chart = new CanvasJS.Chart('chartContainer', {
-    animationEnabled: true,
-    
-    title:{
-      text:'Fortune 500 Companies by Country'
-    },
-    axisX:{
-      interval: 1
-    },
-    axisY2:{
-      interlacedColor: 'rgba(1,77,101,.2)',
-      gridColor: 'rgba(1,77,101,.1)',
-      title: 'Number of Companies'
-    },
-    data: [{
-      type: 'bar',
-      name: 'companies',
-      axisYType: 'secondary',
-      color: '#014D65',
-      dataPoints: [
-        { y: 3, label: 'Sweden' },
-        { y: 7, label: 'Taiwan' },
-        { y: 5, label: 'Russia' },
-        { y: 9, label: 'Spain' },
-        { y: 7, label: 'Brazil' },
-        { y: 7, label: 'India' },
-        { y: 9, label: 'Italy' },
-        { y: 8, label: 'Australia' },
-        { y: 11, label: 'Canada' },
-        { y: 15, label: 'South Korea' },
-        { y: 12, label: 'Netherlands' },
-        { y: 15, label: 'Switzerland' },
-        { y: 25, label: 'Britain' },
-        { y: 28, label: 'Germany' },
-        { y: 29, label: 'France' },
-        { y: 52, label: 'Japan' },
-        { y: 103, label: 'China' },
-        { y: 134, label: 'US' }
-      ]
-    }]
-  });
-  chart.render();
   return canvasJSConfigObject;
 } 
 
@@ -56,8 +15,50 @@ function runThisWithResultsFromServer(jsonFromServer) {
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
-}
 
+}
+const chart = new CanvasJS.Chart('chartContainer', {
+  animationEnabled: true,
+  
+  title:{
+    text:'Fortune 500 Companies by Country'
+  },
+  axisX:{
+    interval: 1
+  },
+  axisY2:{
+    interlacedColor: 'rgba(1,77,101,.2)',
+    gridColor: 'rgba(1,77,101,.1)',
+    title: 'Number of Companies'
+  },
+  data: [{
+    type: 'bar',
+    name: 'companies',
+    axisYType: 'secondary',
+    color: '#014D65',
+    dataPoints: [
+      { y: 3, label: 'Sweden' },
+      { y: 7, label: 'Taiwan' },
+      { y: 5, label: 'Russia' },
+      { y: 9, label: 'Spain' },
+      { y: 7, label: 'Brazil' },
+      { y: 7, label: 'India' },
+      { y: 9, label: 'Italy' },
+      { y: 8, label: 'Australia' },
+      { y: 11, label: 'Canada' },
+      { y: 15, label: 'South Korea' },
+      { y: 12, label: 'Netherlands' },
+      { y: 15, label: 'Switzerland' },
+      { y: 25, label: 'Britain' },
+      { y: 28, label: 'Germany' },
+      { y: 29, label: 'France' },
+      { y: 52, label: 'Japan' },
+      { y: 103, label: 'China' },
+      { y: 134, label: 'US' }
+    ]
+  }]
+});
+chart.render();
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
