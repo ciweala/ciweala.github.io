23c72ed3dfb55e9587fecd4d0a01a0d34615aeb2
ciweala <58187587+ciweala@users.noreply.github.com>
2020-10-29 21:17:15 -0400

    pulled updates

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -12,7 +12,7 @@ function convertRestaurantsToCategories(restaurantList) {
     result[categoryArray[i]] += 1;
   }
 
-  const reply = Object.keys(result).map((category) {
+  const reply = Object.keys(result).map((category) => {
     y: reuslt[category],
     label: category
   }));
@@ -59,52 +59,6 @@ function runThisWithResultsFromServer(jsonFromServer) {
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
-<<<<<<< HEAD
-
-}
-const chart = new CanvasJS.Chart('chartContainer', {
-  animationEnabled: true,
-  
-  title:{
-    text:'Fortune 500 Companies by Country'
-  },
-  axisX:{
-    interval: 1
-  },
-  axisY2:{
-    interlacedColor: 'rgba(1,77,101,.2)',
-    gridColor: 'rgba(1,77,101,.1)',
-    title: 'Number of Companies'
-  },
-  data: [{
-    type: 'bar',
-    name: 'companies',
-    axisYType: 'secondary',
-    color: '#014D65',
-    dataPoints: [
-      { y: 3, label: 'Sweden' },
-      { y: 7, label: 'Taiwan' },
-      { y: 5, label: 'Russia' },
-      { y: 9, label: 'Spain' },
-      { y: 7, label: 'Brazil' },
-      { y: 7, label: 'India' },
-      { y: 9, label: 'Italy' },
-      { y: 8, label: 'Australia' },
-      { y: 11, label: 'Canada' },
-      { y: 15, label: 'South Korea' },
-      { y: 12, label: 'Netherlands' },
-      { y: 15, label: 'Switzerland' },
-      { y: 25, label: 'Britain' },
-      { y: 28, label: 'Germany' },
-      { y: 29, label: 'France' },
-      { y: 52, label: 'Japan' },
-      { y: 103, label: 'China' },
-      { y: 134, label: 'US' }
-    ]
-  }]
-});
-chart.render();
-=======
   const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
   const options = makeYourOptionsObject(reorganizedData);
   const chart = new CanvasJS.Chart('chartContainer', options);
@@ -112,7 +66,6 @@ chart.render();
 }
 
 // Leave lines 52-67 alone; do your work in the functions above
->>>>>>> b1e807f7d88e39018b9a015ade74fe89bb0c9f40
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
